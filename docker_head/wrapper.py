86f42b7a445722c6cb296d4d407eafaa64c1ea93
h3nnn4n <uber.renan@gmail.com>
2021-12-05 11:01:57 -0300

    Fix server address string + logger improvements

--- docker_head/wrapper.py
+++ docker_head/wrapper.py
@@ -1,10 +1,15 @@
 import os
+import logging
 import socket
 
 from pexpect.popen_spawn import PopenSpawn
 
+logger = logging.getLogger('network_wrapper')
+logger.setLevel(logging.DEBUG)
+
 WRAPEE_PATH = "./agent.py"
-SERVER_ADDRESS = "/var/colosseum/colosseum.socket"
+SOCKET_FILE = os.environ.get("SOCKET_FILE", "colosseum.socket")
+SERVER_ADDRESS = f"/var/colosseum/{SOCKET_FILE}"
 SEPARATOR = os.environ.get("SEPARATOR", "\n")
 
 
@@ -23,6 +28,7 @@ def send(socket, msg):
 def reader(socket, read_size=4):
     buffer = ""
     while True:
+        # FIXME: This could deadlock if the message has two separators
         new_data = socket.recv(read_size).decode()
         buffer += new_data
 
@@ -38,16 +44,21 @@ def exchange_message(child_process, data_in):
 
 
 def main():
+    logger.info("starting")
     wrapped = PopenSpawn(WRAPEE_PATH)
     sock = socket.socket(socket.AF_UNIX)
+    logger.info(f"connecting to {SERVER_ADDRESS}")
     sock.connect(SERVER_ADDRESS)
+    logger.info("connected")
 
     while True:
         for data in reader(sock):
+            logger.debug(f"got {data=}")
             result = exchange_message(wrapped, data)
-            if not result:
-                break
+            logger.debug(f"sending {result=}")
             send(sock, result)
+
+    logger.info("done")
 
 
 if __name__ == "__main__":
