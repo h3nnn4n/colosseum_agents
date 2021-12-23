24c51051783294662b8b0a4669f70ae917ee4da5
h3nnn4n <uber.renan@gmail.com>
2021-12-23 12:22:51 -0300

    Update to handle multiple games

--- compress_agents.sh
+++ compress_agents.sh
@@ -3,7 +3,13 @@
 find . -name "*.log" -delete
 find . -name "*.tar.gz" -delete
 
-for d in */ ; do
-  dir_name=`basename "${d}"`
-  tar cf "${dir_name}".tar.gz "${dir_name}"
+for game_dir in */ ; do
+  cd "${game_dir}"
+
+  for agent_dir in */ ; do
+    dir_name=`basename "${agent_dir}"`
+    tar cf "${dir_name}".tar.gz "${dir_name}"
+  done
+
+  cd ..
 done
