d0a13f97fb2064cd42f122d80dbfd3e8936d4c52
h3nnn4n <uber.renan@gmail.com>
2022-01-16 17:25:43 -0300

    Use flask as an standalone wrapper

--- food_catcher/docker_head/agent.py
+++ food_catcher/docker_head/agent.py
@@ -3,50 +3,37 @@
 import logging
 
 from colosseum_sdk import Agent
-from flask import Flask
+
 
 AGENT_NAME = "docker_head"
 
-app = Flask(__name__)
 
-agent = Agent(agent_name=AGENT_NAME)
-
-
-@app.route("/")
 def main():
-    return "<p>Hello, World!</p>"
+    agent = Agent(agent_name=AGENT_NAME)
 
-
-def agent_tick(state_data=None):
-    if state_data is None:
+    while agent.run:
         agent.read_state()
+        state = agent.state
+        main_base = state.bases.mine.first
 
-    state = agent.state
-    main_base = state.bases.mine.first
+        for actor in state.actors.mine:
+            food = state.foods.closest_to(actor)
 
-    for actor in state.actors.mine:
-        food = state.foods.closest_to(actor)
-
-        if actor.distance_to(food) < 0.1:
-            actor.take(food)
-        elif actor.food > 250:
-            base = state.bases.mine.closest_to(actor)
-            if actor.distance_to(base) >= 0.1:
-                actor.move(base)
+            if actor.distance_to(food) < 0.1:
+                actor.take(food)
+            elif actor.food > 250:
+                base = state.bases.mine.closest_to(actor)
+                if actor.distance_to(base) >= 0.1:
+                    actor.move(base)
+                else:
+                    actor.deposit_food(base)
             else:
-                actor.deposit_food(base)
-        else:
-            actor.move(food)
+                actor.move(food)
 
-    if main_base and main_base.can_spawn and state.actors.mine.count < 2:
-        base.spawn()
+        if main_base and main_base.can_spawn and state.actors.mine.count < 2:
+            base.spawn()
 
-    agent.send_commands()
-
-
-def main():
-    while True:
-        agent_tick()
+        agent.send_commands()
 
 
 if __name__ == "__main__":
