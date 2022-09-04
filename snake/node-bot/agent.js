8477e5e26e38ad7f93fb4f16937b00122375b65d
h3nnn4n <git@renan.digital>
2022-09-04 16:54:30 -0300

    Implement basic random agent

--- snake/node-bot/agent.js
+++ snake/node-bot/agent.js
@@ -1,5 +1,35 @@
+const AGENT_NAME = "noodles"
+let AGENT_ID = null
+
+function choice(array) {
+  return array[Math.floor(Math.random() * array.length)];
+}
+
+function update(state, response) {
+  response.move = choice(["UP", "RIGHT", "DOWN", "LEFT"])
+}
+
 process.stdin.on("data", raw_data => {
   raw_data = raw_data.toString()
-  data = JSON.parse(raw_data)
-  process.stdout.write(JSON.stringify(data))
+  state = JSON.parse(raw_data)
+
+  let response = {}
+
+  if (state.stop)
+    process.exit()
+
+  if (state.set_agent_id) {
+    response.agent_name = AGENT_NAME
+    AGENT_ID = state.set_agent_id
+  }
+
+  if (AGENT_ID)
+    response.agent_id = AGENT_ID
+
+  if (state.ping)
+    response.pong = "noodles"
+
+  update(state, response)
+
+  process.stdout.write(JSON.stringify(response))
 })
